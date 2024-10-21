096afe19823034be0bd2e7e3246f964402e37f80
rali717 <Rainer.Spam@posteo.de>
2024-10-21 21:11:28 +0200

    Init

---
+++ CsvView/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CsvView.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ CsvView/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CsvView.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
